4fd14c2611e165e22084a17cecd901ce8715e8af
CarolinaAriadne <c.ariadneadv@gmail.com>
2023-03-16 17:12:45 -0300

    css-fixed

--- frontend/projeto-modelos-de-carros/src/pages/Cars.js
+++ frontend/projeto-modelos-de-carros/src/pages/Cars.js
@@ -142,7 +142,6 @@ export default function CarsPage() {
                 >
                   Editar
                 </button>
-                <section className="container-modal"></section>
               </section>
             );
           })}
